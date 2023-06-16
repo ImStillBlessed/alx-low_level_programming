ce92a34f97850daf7decb1d81bac422b4aa2c1a0
blessed oigbochie <eprince1800@gmail.com>
2023-06-16 13:17:13 -0700

    new

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d;
+	char alpha;
+
+	for (d = '0' ; d <= '9' ; d++)
+		putchar(d);
+	for (alpha = 'a' ; alpha <= 'f' ; alpha++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
