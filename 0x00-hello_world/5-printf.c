5aebdbb20b491f6320e89b689a907956751029f8
blessed oigbochie <eprince1800@gmail.com>
2023-06-16 07:23:17 -0700

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
  * main - Entry point
  * Return: Always o (Success)
  */
-int main(void){
-	printf('with proper grammar, but the outcome is a piece of art,\n')
-	return(0)
+int main(void)
+{
+	printf('with proper grammar, but the outcome is a piece of art,\n');
+	return(0);
 }
