d855665211af64fd12ff8a0bcd73baf81f9976f4
blessed oigbochie <eprince1800@gmail.com>
2023-06-20 23:30:46 -0700

    new commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -prints alphabets 10 times
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 10 ; j > 0 ; j--)
+	{
+		for (i = 'a' ; i <= 'z' ; i++)
+		{
+			putchar(i);
+		}
+		putchar('\n');
+	}
+}
