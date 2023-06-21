c83533f75f878a3c08d196f860e294c44835eb31
blessed oigbochie <eprince1800@gmail.com>
2023-06-21 02:46:33 -0700

    new commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - add values together
+ * @a: first number
+ * @b: second number
+ * return: 0.
+ */
+int add(int a, int b)
+{
+	int num;
+
+	num = a + b;
+
+	return (num);
+}
