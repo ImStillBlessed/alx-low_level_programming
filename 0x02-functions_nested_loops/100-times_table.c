20c3327fb56ec697dde39b81e6fb6b3f01651d9d
blessed oigbochie <eprince1800@gmail.com>
2023-06-21 04:11:22 -0700

    new commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - prints times table
+ * @n: how the times table should be
+ * Return: 0.
+ */
+void print_times_table(int n)
+{
+	int col, row, num;
+
+	if (n > 15 || n < 0)
+		return (0);
+	for (col = 0 ; col <= n ; col++)
+	{
+		for (row = 0 ; row <= n ; row++)
+		{
+			num = row * col;
+			if (num < 100)
+			{
+				if (10 < num)
+				{
+					if (num > 0)
+						printf(",   %d", num);
+					printf("%d", num);
+				}
+				else
+					printf(",  %d", num);
+			}
+			else
+				printf(", %d", num);
+		}
+	}
+}
