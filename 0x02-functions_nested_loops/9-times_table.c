ff5cb74837ca8de9aa914616dec0fc84625dd6be
blessed oigbochie <eprince1800@gmail.com>
2023-06-21 01:43:59 -0700

    new commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - prints times table up to 9
+ * Return: 0.
+ */
+void times_table(void)
+{
+	int r, c, num;
+
+	for (r = 0 ; r <= 9 ; r++)
+	{
+		for (c = 0 ; c <= 9 ; c++)
+		{
+			num = c * r;
+			_putchar('0' + num);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
