0518d43376f17275481ebb505f6fb9677cd04c1d
blessed oigbochie <eprince1800@gmail.com>
2023-06-21 03:22:51 -0700

    new commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,20 +10,38 @@ void print_to_98(int n)
 	{
 		while ( n < 98 )
 		{
-			_putchar('0' + n);
-			_putchar(',');
-			_putchar(' ');
-			n++;
+			if (n > 9)
+			{
+				_putchar('0' + n / 10);
+				_putchar('0' + n % 10);
+				_putchar(',');
+				_putchar(' ');
+				n++;
+			}
+			else
+			{
+				_putchar('0' + n);
+				_putchar(',');
+				_putchar(' ');
 		}
 		while ( n > 98 )
 		{
-			_putchar('0' + n);
-			_putchar(',');
-			_putchar(' ');
-			n--;
+			if (n > 99)
+			{
+				_putchar('0' + n / 100);
+				_putchar('0' + n / 10);
+				_putchar('0' + n % 10);
+				_putchar(',');
+				_putchar(' ');
+				n--;
+			}
+			else
+			{
+				_putchar('0' + 9);
+				_putchar('0' + 9);
+			}
 		}
-		_putchar('98');
 	}
-	else
-		_putchar('98');
+	_putchar('9');
+	_putchar('8');
 }
