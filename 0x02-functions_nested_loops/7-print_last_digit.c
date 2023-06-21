07fb0bd4a6dcd264c11c49925e9a9be4d0862eb3
blessed oigbochie <eprince1800@gmail.com>
2023-06-21 00:49:12 -0700

    new commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * @num: digit to be filtered
+ * Return: 0.
+ */
+int print_last_digit(int num)
+{
+	int j;
+	if (num < 0)
+		num = num * -1
+	j = num % 10;
+	return (j);
+}
