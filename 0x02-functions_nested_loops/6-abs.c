14a4ea782b6a3daffdd9d471197d3ceada26cd51
blessed oigbochie <eprince1800@gmail.com>
2023-06-21 00:41:25 -0700

    new commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - returns the absolute value of parameter
+ * @j: parameter to be checked
+ * Return: 0.
+ */
+int _abs(int)
+{
+	int j;
+
+	if (j < 0)
+		j = j * -1;
+	return (j);
+}
