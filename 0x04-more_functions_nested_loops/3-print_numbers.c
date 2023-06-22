a92ae04d720275e68daf907694b917ade9ddb14a
blessed oigbochie <eprince1800@gmail.com>
2023-06-22 05:48:38 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * Return: 0.
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (int i = 0 ; 1 < 10 ; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
