4926ec71889a99a0489eb80ba25e7cc723e9706b
blessed oigbochie <eprince1800@gmail.com>
2023-06-22 05:29:44 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digits
+ * @c: digit
+ * Return: 0.
+ */
+int _isdigit(char c)
+{
+	if (c >= 48 && c <= 97)
+		return (1);
+	else
+		rerturn (0);
+}
