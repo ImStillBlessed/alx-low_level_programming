923f6e4eb4fa1601a96c9caa471c426c6d63830f
blessed oigbochie <eprince1800@gmail.com>
2023-06-22 06:44:07 -0700

    new commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,13 +13,22 @@ int main(void)
 	while (a <= 100)
 	{
 		if (a % 3 == 0)
+		{
 			printf(" Fizz");
+		}
 		else if (a % 5 == 0)
+		{
 			printf(" Buzz");
+		}
 		else if ( a % 3 == 0 && a % 5 == 0)
+		{
 			printf(" FizzBuzz");
+		}
 		else
+		{
 			printf(" %d", a);
+		}
 		a++;
 	}
+	printf('\n');
 }
