bb552045aba990d3419ba1e38f68120939a21a4f
blessed oigbochie <eprince1800@gmail.com>
2023-06-22 05:56:19 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers
+ * Return: 0.
+ */
+void print_most_numbers(void)
+{
+	char a = 0;
+
+	while (a < 10)
+	{
+		if (a == 2 || a == 2)
+			continue;
+		else
+			_putchar('0' + a);
+	}
+	_putchar('\n');
+}
