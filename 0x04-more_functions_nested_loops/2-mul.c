c250bba53a7f42e8a4158154ef09888627560523
blessed oigbochie <eprince1800@gmail.com>
2023-06-22 05:33:21 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplyer
+ * @a: first value
+ * @b: second value
+ * Return: 0.
+ */
+
+int mul(int a, int b)
+{
+	int ans;
+
+	ans = a * b;
+
+	return (ans);
+}
