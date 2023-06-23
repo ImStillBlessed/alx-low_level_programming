0106c47ac10abbbeb7aac2fc7fc1adf980762d0b
blessed oigbochie <eprince1800@gmail.com>
2023-06-23 06:40:10 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_number - prints integer
+ * @n: number to be printed
+ * Return: void
+ */
+void print_number(int n)
+{
+
+	if (n < 0)
+	{
+		n * -1;
+		_putchar('-');
+	}
+	if (n < 1000)
+	{
+		_putchar('0' + n / 100);
+		n = n % 100;
+	}
+	if (n < 100)
+	{
+		_putchar('0' + n / 10);
+		n = n % 10;
+	}
+	if (n < 10)
+	{
+		_putchar('0' + n);
+	}
+}
