912d9f9a22c540891ed25fdb45a495d4786cd0aa
blessed oigbochie <eprince1800@gmail.com>
2023-06-23 04:44:10 -0700

    new commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,25 +7,24 @@
  */
 void print_triangle(int size)
 {
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	if (size <= 0)
+	{
+	_putchar('\n');
+	return;
+	}
 
-    for (int row = 1; row <= size; row++)
-    {
-        for (int space = size - row; space > 0; space--)
-        {
-            _putchar(' ');
-        }
+	for (int row = 1; row <= size; row++)
+	{
+		for (int space = size - row; space > 0; space--)
+		{
+			_putchar(' ');
+		}
 
-        for (int hash = 1; hash <= row; hash++)
-        {
-            _putchar('#');
-        }
+		for (int hash = 1; hash <= row; hash++)
+		{
+			_putchar('#');
+		}
 
-        _putchar('\n');
-    }
+		_putchar('\n');
+	}
 }
-
