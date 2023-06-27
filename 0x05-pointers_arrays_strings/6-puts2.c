aa61e680d8f5e0eb34c2126c48770dde40a92eb6
ImStillBlessed <106008185+ImStillBlessed@users.noreply.github.com>
2023-06-27 21:15:50 +0100

    Update 6-puts2.c
    new commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,15 @@
  */
 void puts2(char *str)
 {
-  int index = 0;
+	int i = 0;
 
-	while (str[index] != '\0')
+	while (str[i] != '\0')
 	{
-    if (index % 2 != 0)
-      _putchar(*str);
-    index++;
-  }
-  _putchar('\n');
+		if (i == 0)
+			_putchar(str[0])
+		else if (i % 2 = 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
