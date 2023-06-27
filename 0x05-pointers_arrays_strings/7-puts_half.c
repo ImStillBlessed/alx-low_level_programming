208bc97267001f4bcd3707311655a217a3c5276f
ImStillBlessed <106008185+ImStillBlessed@users.noreply.github.com>
2023-06-27 21:37:53 +0100

    new commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts-half - divides and prints half of a string
+ * @str: string input
+ * Return: void.
+ */
+void puts_half(char *str)
+{
+  int i = 0, len = _strlen(str);
+
+  if (len % 2 == 0)
+  {
+    i = len / 2;
+  }
+  else
+  {
+    i = (len - 1) / 2
+  }
+  while (str[i] != '\0')
+    {
+      _putchar(str[i]);
+      i++;
+    }
+  _putchar('\n');
+}
