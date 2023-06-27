ca6b4757eba4145bcd69af92aff1185156512aa3
ImStillBlessed <106008185+ImStillBlessed@users.noreply.github.com>
2023-06-27 22:35:21 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,10 +12,10 @@ void print_array(int *a, int n)
 
   while (i <= n)
     {
-      printf("%s", a[i]);
+      printf("%d", a[i]);
       if (i != n)
       {
-        printf(', ');
+        printf(", ");
       }
       i++;
     }
