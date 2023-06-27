3b3d4aef723241da1ae1b9a1aba46be8bcf820db
ImStillBlessed <106008185+ImStillBlessed@users.noreply.github.com>
2023-06-27 20:01:40 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,16 @@
  */
 void print_rev(char *s)
 {
+	int i = 0;
 	while (*s != '\0')
 	{
+		i++;
 		s++;
-		print_rev(s);
-		_putchar(*s);
+	}
+	while (i > 0)
+	{
+		_putchar(s[i]);
+		i--;
 	}
 	_putchar('\n');
 }
