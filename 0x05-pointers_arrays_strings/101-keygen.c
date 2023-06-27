f6efbabc3626e9acc58df21efe0aae75b792d966
ImStillBlessed <106008185+ImStillBlessed@users.noreply.github.com>
2023-06-27 23:40:13 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,29 +2,53 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 10
-#define NUM_PASSWORDS 10
+/**
+ * main - Generates random valid passwords for the
+ *        program 101-crackme.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-int i;
+	srand(time(0));
 
-char generateRandomChar() {
-    return (char)(rand() % 94 + 33);
+	while (sum < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
+	}
+
+	printf("%s", password);
+
+	return (0);
 }
-
-void generatePassword(char *password) {
-    for (i = 0 ; i < PASSWORD_LENGTH ; i++) {
-        password[i] = generateRandomChar();
-    }
-    password[PASSWORD_LENGTH] = '\0';
-}
-
-char** generatePasswords() {
-    srand(time(NULL));
- 		char** passwords = (char**)malloc(NUM_PASSWORDS * sizeof(char*));
-    for (int i = 0; i < NUM_PASSWORDS; i++) {
-        passwords[i] = (char*)malloc((PASSWORD_LENGTH + 1) * sizeof(char));
-        generatePassword(passwords[i]);
-    }
-
-    return passwords;
-} 
