ae7a0db0dce8de68fdd64ed6639526d65d305978
oigbochie blessed <eprince1800@gmail.com>
2023-06-29 23:11:00 +0100

    new

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,30 @@
 #include "main.h"
+
 /**
- * cap_string - capitalises a string
+ * cap_string - capitalizes a string
  * @str: string to be capped
  * Return: new string
-*/
+ */
 char *cap_string(char *str)
 {
     int i = 0;
+
     while (str[i])
     {
         while (!(str[i] >= 'a' && str[i] <= 'z'))
             i++;
+
         if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
             str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
             str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
             str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
             str[i - 1] == '}')
+        {
             str[i] -= 32;
-        i++; 
+        }
+
+        i++;
     }
+
     return str;
 }
