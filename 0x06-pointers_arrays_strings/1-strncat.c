2f698241058976b1610a60fc5276cbe1371c3dd2
oigbochie blessed <eprince1800@gmail.com>
2023-06-29 20:14:12 +0100

    new commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - concats specific numbers of strings
+ * @src: source string
+ * @dest: new string
+ * @n: number of characters to append
+ * Return: new string variable
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+    int a = 0, i = 0;
+
+    while (dest[i] != '\0')
+    {
+        i++;
+    }
+    
+
+    while (n > 0)
+    {
+        dest = dest[i] + src[a];
+        a++;
+        n--;
+    }
+    
+    return (dest);
+
+}
