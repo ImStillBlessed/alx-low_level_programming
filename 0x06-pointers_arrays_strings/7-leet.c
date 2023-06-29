8a8d92fe531904b4380b753b03c14bccb47aeaa5
oigbochie blessed <eprince1800@gmail.com>
2023-06-29 23:26:06 +0100

    first commit in leet without any additional help

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - codes to 1337
+ * @str: string to ecode
+ * Return: the encoded string
+*/
+char *leet(char *str)
+{
+    int a = 0;
+    int i;
+    char letters[10] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
+    char nums[10] = {4, 3, 0, 7, 1, 4, 3, 0, 7, 1};
+
+    while (str[a])
+    {
+        for (i = 0; i < 10; i++)
+        {
+            if (str[a] == letters[i])
+            {
+                str[a] = nums[i];
+            }
+        }
+        a++;
+    }
+}
