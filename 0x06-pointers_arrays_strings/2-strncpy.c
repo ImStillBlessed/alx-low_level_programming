0671609c268c0d224d7a9114670eae0b207a8864
oigbochie blessed <eprince1800@gmail.com>
2023-06-29 21:21:33 +0100

    new commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strcpy - copies strings
+ * @src: first
+ * @dest: second
+ * @n: number of characters
+ * Return: void.
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+    int a = 0, i = 0;
+
+    while (src[i] != '\0')
+        i++;
+    
+    while (a <= n)
+    {
+        if (a <= i)
+        {
+            dest[a] = src[a];
+            a++;
+        }
+        else
+        {
+            break;
+        }
+        
+    }
+    return dest;    
+}
