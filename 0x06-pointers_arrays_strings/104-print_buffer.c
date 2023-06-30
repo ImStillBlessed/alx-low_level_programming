c8c7c851ed3bb70569a60fa79024318661e8b453
oigbochie blessed <eprince1800@gmail.com>
2023-06-30 22:41:20 +0100

    new commit

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer
+ * @size: size of the buffer
+ * Return: void.
+ */
+
+void print_buffer(char *b, int size)
+{
+	// I've got no idea what i am supposed to do 
+	// This sucks balls 
+	// just fuckinghatye myself right now!!!!!
+
+	_putchar("0" + 0);
+	return 0;
+}
