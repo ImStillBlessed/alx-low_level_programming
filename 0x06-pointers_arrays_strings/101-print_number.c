de0cc4a5a3fce0776c4d267be995e1c4e3377314
oigbochie blessed <eprince1800@gmail.com>
2023-06-30 16:08:22 +0100

    new

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,14 +6,14 @@
 */
 void print_number(int n)
 {
-        if (n < 0)
-        {
-                n = -n;
-                _putchar('-');
-        }
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
 
-        if (n / 10 != 0)
-                print_number(n / 10);
+	if (n / 10 != 0)
+		print_number(n / 10);
 
-        _putchar('0' + n % 10); 
+	_putchar('0' + n % 10); 
 }
