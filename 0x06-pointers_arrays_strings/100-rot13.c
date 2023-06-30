0ca746433971b175c613b014b0a0eea850a90da9
oigbochie blessed <eprince1800@gmail.com>
2023-06-30 15:14:04 +0100

    new commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string to rot 13
+ * @str: string to be encodeed
+ * Returm: encoded string
+*/
+char *rot13(char *str)
+{
+    int *ptr = str;
+    char alpha[] = 'NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm';
+
+    while (*ptr != '\0')
+    {
+        int is_upper = (*ptr >= 'A' && *ptr <= 'Z');
+        int is_lower = (*ptr >= 'a' && *ptr <= 'z');
+
+        if (is_upper || is_lower)
+        {
+            char *alpha_ptr = alpha + ((*ptr & 31) - 1 + (is_lower * 26));
+
+            *ptr = *alpha_ptr;
+        }
+        ptr++;
+    }
+    return str;
+    
+}
