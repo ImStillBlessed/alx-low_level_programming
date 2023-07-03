f507fa7775299c560b8252c21964f71e08e2f41d
ImStillBlessed <106008185+ImStillBlessed@users.noreply.github.com>
2023-07-03 23:23:12 +0100

    Update 6-print_chessboard.c

--- 0x07-pointers_arrays_strings/6-print_chessboard.c
+++ 0x07-pointers_arrays_strings/6-print_chessboard.c
@@ -1,1 +1,21 @@
 #include "main.h" 
+/**
+* print_chessboard - prints the pieces on a chessboard
+* @a: the board area
+* Return: void. 
+*/
+void print_chessboard(char (*a)[8])
+{
+  int i, j;
+
+  while (i < 8)
+    {
+      j = 0;
+      while (j < 8)
+        {
+          _putchar(a[i][j]);
+          j++;
+        }
+      i++;
+    } 
+}
