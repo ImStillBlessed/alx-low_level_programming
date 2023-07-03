f8a2b717a0129c7cedffff8be2f84981130de056
ImStillBlessed <106008185+ImStillBlessed@users.noreply.github.com>
2023-07-03 17:16:06 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,6 +2,20 @@
 /**
 * _strchr - checks for string occurance and prints what's left
 * @s: string to check
-* @n: letter to check for
+* @c: character to check for
 * Return: new string.
 */
+char *_strchr(char *s, char c)
+{
+  int i;
+	char temp = s;
+
+  while (s != '\0')
+  {
+    if (!(s[i] == c)) 
+    {
+      temp[i] 
+    }
+      
+  } 
+}
