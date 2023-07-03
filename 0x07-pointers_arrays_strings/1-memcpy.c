332425d1143f9abc70f276fcaaab3a771f86083c
ImStillBlessed <106008185+ImStillBlessed@users.noreply.github.com>
2023-07-03 16:55:29 +0100

    Create 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _memcpy - copies data from @src to @dest
+* @src: source pointer with strings to copy
+* @dest: buffer tk copy the data to
+* @n: amount of @src to copy
+* Return: pointer to the new buffer. 
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+  unsigned int i = 0;
+
+  while (i < n)
+  {
+    dest[i] = src[i];
+    i++;
+  } 
+}
