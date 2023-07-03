6a3e50fcb70ff7a0e8f54522ba642d3865b02059
ImStillBlessed <106008185+ImStillBlessed@users.noreply.github.com>
2023-07-03 23:48:00 +0100

    Update 8-print_diagsum.c

--- 0x07-pointers_arrays_strings/8-print_diagsum.c
+++ 0x07-pointers_arrays_strings/8-print_diagsum.c
@@ -7,16 +7,13 @@
 
 void print_diagsums(int *a, int size)
 {
-  int i = 0;
+  int i, j;
   int val1 = 0;
   int val2 = 0;
 
   for (i = 0; i < size; i++)
       val1 += a[i][i];
-  for (j = size - 1, i = 0; i >= 0)
-    {
-      val2 += a[i][i];
-      i--;
-    }
+  for (j = size - 1, i = 0; j >= 0; j--, i++)
+      val2 += a[i][j];
   printf("%d %d", val1, val2);
 }
