753ff3f69f2c2ef202ebbb6dcd7dc2850298af14
ImStillBlessed <106008185+ImStillBlessed@users.noreply.github.com>
2023-07-03 19:48:39 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,1 +1,8 @@
 #include "main.h"
+/**
+* _strspn - returns the number of bytes when a character not in 
+* original string is found
+* @s1: first string to check
+* @s2: second string of values to check
+* Return: the bytes of value when unknown found
+*/
