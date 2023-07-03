1b25fd9b7066614a904d756b9f74d1fd63851c5e
ImStillBlessed <106008185+ImStillBlessed@users.noreply.github.com>
2023-07-03 19:59:56 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,25 +1,29 @@
 #include "main.h"
+
 /**
 * _strspn - returns the number of bytes when a character not in 
 * original string is found
-* @s1: first string to check
-* @s2: second string of values to check
+* @s: first string to check
+* @accept: second string of values to check
 * Return: the bytes of value when unknown found
 */
 unsigned int _strspn(char *s, char *accept)
 {
-  for (int i = 1 ; s ; s++)
-  {
-		while (accept)
-      {
-        if (accept != s) 
-          return i;
-        else
-        {
-          i++;
-          accept++
-        }
-      } 
-  } 
+  unsigned int i = 0;
 
+  for (; *s != '\0'; s++)
+  {
+    for (; *accept != '\0'; accept++)
+    {
+      if (*accept != *s) 
+        return i;
+      else
+      {
+        i++;
+        accept++;
+      }
+    } 
+  } 
+  
+  return i;
 }
