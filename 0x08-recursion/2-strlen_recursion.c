8c233da7abc290b4cde5e056d029ec89f9ffeee2
ImStillBlessed <Eprince1800@gmail.com>
2023-07-05 12:00:20 +0100

    created strlen with recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the lenght of a string
+ * @s: the string to count
+ * Return: the integer lenght of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int k = 0;
+
+	if (*s == '\0')
+		return k;
+	else
+	{
+		k++;
+		k += _strlen_recursion(s + 1);
+		return k;
+	}
+}
