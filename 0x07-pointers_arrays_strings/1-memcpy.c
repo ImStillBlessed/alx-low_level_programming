bf13082b937b25fe916d27decc3cb41e4fa9a82d
ImStillBlessed <106008185+ImStillBlessed@users.noreply.github.com>
2023-07-06 12:08:39 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,7 +4,7 @@
 * @src: source pointer with strings to copy
 * @dest: buffer tk copy the data to
 * @n: amount of @src to copy
-* Return: pointer to the new buffer. 
+* Return: pointer to the new buffer.
 */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
