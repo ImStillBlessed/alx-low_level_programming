cf363592e7bc2087f83149e2cee388d1f0b5e14f
ImStillBlessed <106008185+ImStillBlessed@users.noreply.github.com>
2023-07-08 03:57:53 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
 * main - Entry point
 * @argc: number of arguments
 * @argv: arguments
 * Return: 0.
 */
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
 	int sum = 0;
 	int i;
