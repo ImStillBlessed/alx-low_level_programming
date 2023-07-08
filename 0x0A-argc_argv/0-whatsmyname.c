44ac1e21dfdd11dc192a729b2b2fb7eab3e67b40
ImStillBlessed <106008185+ImStillBlessed@users.noreply.github.com>
2023-07-08 01:44:38 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+/**
+* main - Entry point
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: void.
+*/
+void main(int argc, char *argv[])
+{
+  printf(argv[0]);
+  return (0);
+}
