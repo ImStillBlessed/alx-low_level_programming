7b6ccec7a19b22ad46de0fc3a86ba35f8d7cd6b3
ImStillBlessed <106008185+ImStillBlessed@users.noreply.github.com>
2023-07-08 03:48:56 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,4 +9,18 @@ int main(int argc, char *argv)
 {
 	int sum = 0;
 	int i;
+
+	if (argc < 2)
+		prinf("%d", 0);
+	else
+		for (i = 1; i < argc; i++)
+		{
+			if (!(argv[i] < 65 && argv[i] > 15))
+			{
+				printf("ERROR\n");
+				return (1);
+			}
+			sum += argv[i];
+			printf("%d\n", sum);
+		}
 } 
