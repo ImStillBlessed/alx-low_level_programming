7cac924b91b46c4c3c668483c67c13863888655a
ImStillBlessed <eprince1800@gmail.com>
2023-08-07 05:38:30 -0700

    new commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - concats 2 strings and returns pointer
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to the new string or NULL.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int one, two, total;
+	int i = 0;
+	char *n_str;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	one = strlen(s1);
+	two = strlen(s2);
+	total = one + two + 1;
+
+	n_str = malloc(sizeof(char) * total);
+	while (i < total)
+	{
+		if (n_str == NULL)
+			return (NULL);
+		if (i <= one)
+			n_str[i] = s1[i];
+		else
+		for (j = 0; j <= two; j++)
+		{
+			n_str[i] = s2[j];
+			i++;
+		}
+		i++;
+	}
+	return (n_str);
+}
+
