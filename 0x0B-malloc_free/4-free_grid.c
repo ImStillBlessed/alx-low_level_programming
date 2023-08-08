1971b2c465786042e46e9761eb7aea41f7994544
ImStillBlessed <eprince1800@gmail.com>
2023-08-08 02:39:47 -0700

    literally took 3mins, not sure it would work tho :/

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - frees a 2 dimentional grid mem allocatrd by malloc
+ * @grid: input grid.
+ * @height: grid height.
+ * Return: a nothing but a freed memory.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	free(grid);
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	return (0);
+}
