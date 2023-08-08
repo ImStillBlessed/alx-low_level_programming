5dbd52c4bebdb8545165867113207f71a808151a
ImStillBlessed <eprince1800@gmail.com>
2023-08-08 01:59:29 -0700

    finally completed the alloc grid shii..

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,26 +12,29 @@
 int **alloc_grid(int width, int height)
 {
 	int **new_grid;
-	int all;
 	int i = 0;
 	int j = 0;
+	int k = 0;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	all = width * height;
-	new_grid = malloc(sizeof(int) * all);
+	new_grid = malloc(sizeof(int *) * height);
 	if (new_grid == NULL)
 	{
 		return (NULL);
-		free(new_grid);
 	}
-	while (i <= height)
-	{
-		while (j <= width)
+	for (i = 0; i < height; i++)
+	}
+		new_grid[i] = malloc(sizeof(int) * width);
+		if (new_grid[i] == NULL)
 		{
-			new_grid[i][j] = '0';
-			j++;
+			for (k = 0; k < i; k++)
+				free(new_grid[k]);
+			free(new_grid);
+			return (NULL);
 		}
+		for (j = 0; j < width; j++)
+			new_grid[i][j] = 0;
 		i++;
 	}
 	return (new_grid);
