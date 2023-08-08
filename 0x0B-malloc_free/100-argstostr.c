dc051f475a8cc5170befcd4d5afb6df00d020bba
ImStillBlessed <eprince1800@gmail.com>
2023-08-08 02:58:51 -0700

    there is no way it was this simple

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * argstostr - prints terminal arguments as strings
+ * @ac: argument counter
+ * @av: argument variable (array)
+ * Return: print the arguments or return NULL.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i;
+
+	if (ac == NULL || av == NULL)
+		return (NULL);
+	for (i = 1; i < ac; i++)
+	{
+		printf("%s\n", av[i]);
+	}
+}
