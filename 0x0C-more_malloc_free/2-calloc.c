bedb5964f5369a004a5d79d36bfa7cd8284fb2af
ImStillBlessed <eprince1800@gmail.com>
2023-08-10 07:39:46 -0700

    commit 1

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _calloc - mimicing calloc(), will allocate memory and set to 0.
+ * @nmemb: number of elements in the array
+ * @size: size in bytes of elements.
+ * Return: pointer to the new array, NULL if fail.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	void *array;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	array = malloc(nmemb * size);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i <= nmemb; i++)
+	{
+		array[i] = 0;
+	}
+	return (array);
+}
