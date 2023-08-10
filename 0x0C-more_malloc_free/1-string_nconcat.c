dbf7fedb2a2739c0d0eb19b298b9890a90bbde67
ImStillBlessed <eprince1800@gmail.com>
2023-08-10 07:18:23 -0700

    commit 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * string_nconcat - concats @n number of a second string to the first
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of s2 to use.
+ * Return: pointer to new string or NULL if failed.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *out;
+	int i, start;
+
+	if (s1 == NULL)
+		start = 1;
+	else
+		start = strlen(s1);
+
+	out = malloc(start + n + 1);
+	if (out == NULL)
+		return (NULL);
+
+	for (i = 0; i < start; i++)
+		out[i] = s1[i];
+
+	for (i = 0; i < n; i++)
+	{
+		if (s2[i] == ' ')
+			return (out);
+		out[start + i] = s2[i];
+	}
+	return (out);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,2 +1,3 @@
 void *malloc_checked(unsigned int b);
 char _putchar(char *str);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
