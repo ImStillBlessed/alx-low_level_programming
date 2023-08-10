6768aefbe2b6b3eba2dcf3ee69569b1fb47817b9
ImStillBlessed <eprince1800@gmail.com>
2023-08-10 08:47:35 -0700

    hmm...

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	array = (char *)malloc(nmemb * size);
 	if (array == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < size / sizeof(char); i++)
 	{
 		array[i] = 0;
 	}
