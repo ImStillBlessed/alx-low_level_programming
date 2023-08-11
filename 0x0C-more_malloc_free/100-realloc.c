bbb6740f8630394b02f84319286a52dee84e153c
ImStillBlessed <106008185+ImStillBlessed@users.noreply.github.com>
2023-08-11 11:34:13 +0100

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,6 +14,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *array;
 
+	if (new_size == old_size)
+		return (ptr);
 	if (new_size == 0 && !(ptr == NULL))
 	{
 		free(ptr);
