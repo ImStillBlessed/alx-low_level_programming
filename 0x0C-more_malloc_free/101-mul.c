b34f71523b85c5298f2f1159104454fe2057e7f4
oigbochie Blessed <eprince1800@gmail.com>
2023-08-11 12:28:25 +0100

    testing the new isdigit loop

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "main.h"
+#include <ctype.h>
 
 /**
  * main - Entry point
@@ -11,15 +12,49 @@
  */
 int main(int ac, char **av)
 {
-	int ans, first, second;
+	int ans, first, second, i;
 
 	if (ac > 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	first = atoi(av[1]);
-	second = atoi(av[2]);
+
+	if (strlen(av[1]) > 0)
+	{
+		for (i = 0; av[1][i]; i++)
+		{
+			if (!isdigit(av[1][i]))
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+		first = atoi(av[1]);
+	}
+	else
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (strlen(av[2]) > 0)
+	{
+		for (i = 0; av[2][i]; i++)
+		{
+			if (!isdigit(av[2][i]))
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+		second = atoi(av[2]);
+	}
+	else
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	ans = first * second;
 	printf("%d\n", ans);
 	return (0);
