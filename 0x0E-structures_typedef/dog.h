ac507cbfe07320dfaf54de171210fdc209c761e1
oigbochie Blessed <eprince1800@gmail.com>
2023-08-14 16:15:16 +0100

    new commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,11 @@
+/**
+ * struct dog - houses dog
+ * @name: hus name
+ * @age: his age in years
+ * @owner: the person with leagal ornership
+ * Description: houses a dog and its details.
+ */
+
 struct dog
 {
 	char *name;
