faec5a8118edf54d80bff89b58e53ce830ae0844
oigbochie Blessed <eprince1800@gmail.com>
2023-08-15 16:48:45 +0100

    latest commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+#include <string.h>
+
+/**
+ * new_dog - copy of my_dog
+ * @name: same name
+ * @age: float of same age
+ * @owner: smae owner as my_dog
+ * Description: returns a pointer to new_dog or NULL 
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = strdup(name);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->age = age;
+
+	new_dog->owner = strdup(owner);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->owner);
+		free(new_dog);
+		return (NULL);
+	}
+	return (new_dog);
+}
