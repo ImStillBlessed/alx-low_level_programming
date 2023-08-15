6f9f7342f79c79548e6c6ca57d48d2bd5cecdb9b
oigbochie Blessed <eprince1800@gmail.com>
2023-08-15 07:43:10 +0100

    new commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,13 +14,13 @@ void print_dog(struct dog *d)
 	if (d->name != NULL)
 		printf("Name: %s\n", d->name);
 	else
-		printf("Name: nil");
+		printf("Name: nil\n");
 	if (d->age == 0)
-		printf("Age: nil");
+		printf("Age: nil\n");
 	else
 		printf("Age: %f\n", d->age);
 	if (d->owner != NULL)
 		printf("Owner: %s\n", d->owner);
 	else
-		printf("Owner: nil");
+		printf("Owner: nil\n");
 }
