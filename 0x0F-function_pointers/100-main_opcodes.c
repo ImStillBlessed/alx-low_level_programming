d9a038abfbba54231cdfd894ee96e348dfde537f
oigbochie Blessed <eprince1800@gmail.com>
2023-08-17 11:56:55 +0100

    new commit

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+void print_opcodes(int num_bytes);
+
+/**
+ * print_opcodes - pri ts the opcode for the function
+ * @num_bytes: number of bytes as inout
+ * Return: nothing.
+ */
+
+void print_opcodes(int num_bytes)
+{
+	void (*ptr)() = print_opcodes;
+
+	for (int i = 0; i < num_bytes; i++)
+	{
+		unsigned char opcode = *((unsigned char*)ptr + i);
+		printf("%02x", opcode);
+	}
+
+	printf("\n");
+}
+
+/**
+ * main - Entry point
+ * @argc: variable count
+ * @argv: variable array.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int num_bytes = atoi(argv[1]);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return 1;
+	}
+	if (num_bytes <= 0)
+	{
+		printf("Error\n");
+		return 2;
+	}
+
+	print_opcodes(num_bytes);
+
+	return 0;
+}
