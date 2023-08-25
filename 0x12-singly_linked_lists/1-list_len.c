075c926b05570d92b330f1780afcf814ce3b45d4
Blessed Oigbochie <eprince1800@gmail.com>
2023-08-25 13:04:18 +0100

    made the list_len file

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - counts the lenght of a list
+ * @h: pointer to the first element of the list
+ * Return: the number of elements on the list.
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	list_t *ptr = h;
+
+	if (h == NULL)
+		return (0);
+	while (ptr != NULL)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+	return (count);
+}
