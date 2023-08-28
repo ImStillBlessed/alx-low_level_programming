9fb1f971aff42cf818d75f49d50d322bf97b9eae
Blessed Oigbochie <eprince1800@gmail.com>
2023-08-28 22:52:36 +0100

    made the sum function

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_listint - sums all the integer data on the list
+ * @head: pointer to the first element on the list
+ *
+ * Return: the sum of all the integers.
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *ptr;
+	int i;
+
+	if (head == NULL)
+		return (NULL);
+	ptr = head;
+	while (ptr != NULL)
+	{
+		i += ptr->n;
+		ptr = ptr->next;
+	}
+	return (i);
+}
