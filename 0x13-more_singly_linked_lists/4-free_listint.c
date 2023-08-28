2cbbbfa0fca9d61c5f5c2e30817326c922a6ccb7
Blessed Oigbochie <eprince1800@gmail.com>
2023-08-28 19:17:17 +0100

    made the free function to free the list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees memory allocated for the list of integers
+ * @head: pointer to the first node on the list
+ *
+ * Return: nothing.
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr, *temp;
+
+	if (head = NULL)
+	{
+		exit(0);
+		free(head);
+	}
+	ptr = head;
+	while (ptr != NULL)
+	{
+		temp = ptr;
+		ptr = temp->next;
+		free(temp);
+	}
+	free(ptr);
+}
