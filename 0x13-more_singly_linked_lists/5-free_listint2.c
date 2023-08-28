c170e38769fc1057fb577e132d09f0f726b8330d
Blessed Oigbochie <eprince1800@gmail.com>
2023-08-28 19:28:53 +0100

    testing the new free function

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list but sets its first node to 
+ * @head: pointer to the pointer of the first node
+ *
+ * Return: nothing.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr, *temp;
+
+	if (*head == NULL)
+		return;
+	else
+	{
+		ptr = *head;
+		while (ptr != NULL)
+		{
+			temp = ptr;
+			ptr = temp->next;
+			free(temp);
+		}
+		free(ptr);
+	}
+	*head = NULL;
+}
