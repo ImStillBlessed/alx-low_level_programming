df9bbd21c1e77fc8afbc6b2f229eda5eb26e7284
Blessed Oigbochie <eprince1800@gmail.com>
2023-09-02 15:58:03 +0100

    simple solution

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,8 +7,10 @@ void print_binary(unsigned long int n)
 	if (n == 0 || n == 1)
 	{
 		putchar(n + '0');
-		exit(0);
 	}
-	print_binary(n >> 1);
-	putchar((n & 1) + '0');
+	else
+	{
+		print_binary(n >> 1);
+		putchar((n & 1) + '0');
+	}
 }
