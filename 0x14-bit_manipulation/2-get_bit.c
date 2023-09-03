2826e13471686d9d0ef035d198a627efe7db86ae
ImStillBlessed <eprince1800@gmail.com>
2023-09-03 15:32:38 -0700

    made a simple fix for he get bit function

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * get_bit - loops through the bit number ti find the number @index
+ * get_bit - loops through the bit number to find the number @index
  * @n: the number input
  * @index: the inddx of the return nunber
  * Return: the number '1 or 0' @index
@@ -10,12 +10,8 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int mask;
-	int bit;
-
-	if (index > sizeof(unsigned long int))
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
-	mask = 1UL << index;
-	bit = (n & mask) != 1;
-	return (bit);
+	n = n >> index;
+	return (n & 1);
 }
