fa8f52b356b4bc4dc94428ecefb2eb020bcdecce
ImStillBlessed <eprince1800@gmail.com>
2023-09-03 15:56:43 -0700

    first run for the clear bit function without any tests or checks;

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the bit at a given @index to 0
+ * @n: the number to set
+ * @index: index to set a 0
+ * Return: 1 on success or -1 if failed.
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+	mask = 1UL << index;
+	*n = *n ~ mask;
+	return (1);
+}
