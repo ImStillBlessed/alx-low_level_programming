1e0474666de16613ca16dd95ae3e0ff9b37fed2a
ImStillBlessed <eprince1800@gmail.com>
2023-09-03 16:20:13 -0700

    new commit on flip bit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - calculates how many flips to get from one number to another
+ * @n: first number
+ * @m: seconf number
+ * Return: 1 if successful, -1 if failed
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int result;
+	unsigned int count = 0;
+
+	result = n ^ m;
+
+	while (result > 0)
+	{
+		count += result & 1;
+		result >>= 1;
+	}
+	return (1);
+}
