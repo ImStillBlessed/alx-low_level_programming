a96779b3377f95a49b3d0554db5eeabe65bead7e
Blessed Oigbochie <eprince1800@gmail.com>
2023-09-06 06:57:34 +0100

    commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,31 +12,18 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int filed;
-ssize_t file_rd = 0;
-char *buffer;
+	FILE *file;
+	char ch[10000];
+	size_t bytes;
 
-if (filename == NULL)
-return (0);
-filed = open(filename, O_RDONLY);
-if (filed == -1)
-return (0);
-buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL)
-{
-close(filed);
-return (0);
-}
-file_rd = read(filed, buffer, letters);
-if (file_rd == -1)
-{
-close(filed);
-free(buffer);
-return (0);
-}
-write(STDOUT_FILENO, buffer, file_rd);
-free(buffer);
-close(filed);
-return (file_rd);
-}
+	if (filename == NULL)
+		return (0);
+	file = fopen(filename, O_RDONLY);
+	if (file == NULL)
+		return (0);
+	bytes = fread(ch, 1, letters, file);
+	if (bytes > 0)
+		fwrite(ch, 1, bytes, stdout);
+	fclose(file);
+	return (bytes);
 }
