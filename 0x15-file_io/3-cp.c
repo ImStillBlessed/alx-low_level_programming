f7ab4725089c5e7970f4431e147124e87690426c
Blessed Oigbochie <eprince1800@gmail.com>
2023-09-07 08:51:46 +0100

    cp 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 /**
  * main - entry point for the cp functionality
@@ -12,16 +13,17 @@
 int main(int ac, char **av)
 {
 	char str[1024];
+	size_t bytes_read;
+	FILE *fp;
+	FILE *fpi;
+
 	if (ac != 3)
 	{
 		fprintf(stderr, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	FILE *fp;
-	FILE *fpi;
-
-	fp = fopen(av[1], "r");
+	fp = fopen(av[1], "rb");
 	if (fp == NULL)
 	{
 		fprintf(stderr, "Error: Can't read from file %s\n", av[1]);
@@ -31,12 +33,31 @@ int main(int ac, char **av)
 	if (fpi == NULL)
 	{
 		fprintf(stderr, "Error: Can't write to %s\n", av[2]);
+		fclose(fp);
 		exit(99);
 	}
+	while ((bytes_read = fread(str, 1, 1024, fp)) > 0)
+	{
+		if (fwrite(str, 1, bytes_read, fpi) != bytes_read)
+		{
+			fprintf(stderr, "Error: Can't write to %s\n", av[2]);
+			fclose(fp);
+			fclose(fpi);
+			exit(99);
+		}
+	}
 
-	fread(str, 1, 1024, fp);
-	fputs(str, fpi);
 	fclose(fp);
+	if (fclose(fp) != 0)
+	{
+		fprintf(stderr, "Error: Can't close fd %d\n", fileno(fp));
+		exit(100);
+	}
 	fclose(fpi);
+	if (fclose(fpi) != 0)
+	{
+		fprintf(stderr, "Error: Can't close fd %d\n", fileno(fpi));
+		exit(100);
+	}
 	return (0);
 }
