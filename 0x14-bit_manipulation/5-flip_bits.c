e3f853545c8731367fadf603d74b971fa1f80c91
ImStillBlessed <106008185+ImStillBlessed@users.noreply.github.com>
2023-09-13 08:10:09 +0100

    Flip bit test with if condition

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -16,8 +16,9 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	while (result > 0)
 	{
-		count += result & 1;
+		if (result & 1)
+			count++;
 		result >>= 1;
 	}
-	return (1);
+	return (count);
 }
