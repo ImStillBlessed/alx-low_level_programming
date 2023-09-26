a6296bb1f93d3d254abd2ff3bd5f3feda72a12fe
Oigbochie Blessed <eprince1800@gmail.com>
2023-09-26 21:37:22 +0100

    made / fixed the print all functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,11 +5,41 @@
 
 void print_all(const char * const format, ...)
 {
-	char *formats = {"c", "i", "f", "s"};
+	int val = 0;
+	char *str, *seperator = "";
 
-/*
-	va_list(args);
+	va_list args;
 	va_start(args, format);
-*/
-	printf("%s\n", formats);
+
+	if (format)
+	{
+		while (format[val])
+		{
+			switch (format[val])
+			{
+				case 'c':
+					printf("%s%c", seperator, va_arg(args, int));
+					break;
+				case 'i':
+					printf("%s%d", seperator, va_arg(args, int));
+					break;
+				case 'f':
+					printf("%s%f", seperator, va_arg(args, double));
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (!str)
+						str = "nil";
+					printf("%s%s", seperator, str);
+					break;
+				default:
+				val++;
+				continue;
+			}
+			seperator = ", ";
+			val++;
+		}
+	}
+	printf("\n");
+	va_end(args);
 }
