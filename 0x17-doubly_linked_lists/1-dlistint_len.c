ae07bf6cf7a10f7853cd1d8c559228ab84b75221
Blessed Oigbochie <eprince1800@gmail.com>
2023-10-12 18:54:30 +0100

    git

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/*
+ * dlistint_len - prints the count of a lists
+ * @h: pointer to the head of the node
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *current;
+	size_t count;
+
+	if (h == NULL)
+		return (NULL);
+	current = h;
+	count = 0;
+	while (current != NULL)
+	{
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
