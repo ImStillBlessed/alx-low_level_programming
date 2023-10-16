c59c84a032e9272556fe75b62180b8de5f95157d
Blessed Oigbochie <eprince1800@gmail.com>
2023-10-16 13:41:28 +0100

    first dynamic library

---
+++ 0x18-dynamic_libraries/func2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int _isdigit(int c)
+{
+	return (0);
+}
+int _strlen(char *s)
+{
+	return (0);
+}
+void _puts(char *s)
+{}
+char *_strcpy(char *dest, char *src)
+{
+	return (dest);
+}
+int _atoi(char *s)
+{
+	return (0);
+}
