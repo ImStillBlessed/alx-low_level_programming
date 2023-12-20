367116f8ce49e6bf5eff7d1dc78342d7e7e10a09
Oigbochie Blessed <eprince1800@gmail.com>
2023-12-20 21:55:35 +0100

    task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module prints the perimeter of an island
+"""
+
+def island_perimeter(grid):
+    """
+    this function calculates the perimeter if an island represented
+    by '1' in a list in a list of '0's
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                # check right
+                if checker(grid, i, j + 1):
+                    perimeter += 1
+                # check left
+                if checker(grid, i, j - 1):
+                    perimeter += 1
+                # check up
+                if checker(grid, i + 1, j):
+                    perimeter += 1
+                # check down
+                if checker(grid, i - 1, j):
+                    perimeter += 1
+    return perimeter
+
+
+def checker(grid, i, j):
+    if i < 0 or j < 0:
+        return 1
+    try:
+        if grid[i][j] != 1:
+            return 1
+    except:
+        return 1
+    return 0
