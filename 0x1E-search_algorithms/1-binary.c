4f73e81781cb8fdd9ae5badf5b2204214bd0c142
Blessed Oigbochie <eprince1800@gmail.com>
2024-02-05 20:03:02 +0100

    changed the binary search to recursion x

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,6 @@
 #include "search_algos.h"
 
+int helper(int *array, size_t start, size_t end, int value);
 /**
  * binary_search - function to search
  * @array: pointer to first value on the list
@@ -13,33 +14,42 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	int mid;
-	size_t i, start = 0;
-	size_t end = size - 1;
-
 	if (array == NULL)
 		return (-1);
-	while (1)
-	{
-		printf("Searching in array: ");
-		for (i = start ; i <= end ; i++)
-		{
-			printf("%d", array[i]);
-			if (i != end)
-				printf(", ");
-			else
-				printf("\n");
-		}
+	return (helper(array, 0, size - 1, value));
+}
 
-		mid = (start + end) / 2;
-		if (array[mid] == value)
-			return (mid);
-		if (end == start)
-			break;
-		if (value > array[mid])
-			start = mid + 1;
+
+/**
+ * helper - helper function for binary_search.
+ * @array: pointer to the first element on the list.
+ * @start: index of tge first element on the filtered list
+ * @end: last element on filtered list
+ * @value: value to find.
+ * Return: index of @value or -1 on failure.
+ */
+
+
+int helper(int *array, size_t start, size_t end, int value)
+{
+	size_t i, mid = start + (end - start) / 2;
+
+	if (start > end)
+		return (-1);
+	printf("Searching in array: ");
+	for (i = start ; i <= end ; i++)
+	{
+		printf("%d", array[i]);
+		if (i != end)
+			printf(", ");
 		else
-			end = mid - 1;
+			printf("\n");
 	}
+	if (array[mid] == value)
+		return (mid);
+	if (value > array[mid])
+		return (helper(array, mid + 1, end, value));
+	else
+		return (helper(array, start, mid - 1, value));
 	return (-1);
 }
