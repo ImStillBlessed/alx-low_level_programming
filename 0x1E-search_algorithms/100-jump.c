6ce7391b4a6354696fb522b5adfaaadaa155a24e
Oigbochie Blessed <eprince1800@gmail.com>
2024-02-05 21:05:37 +0100

    task 100 the loops are faulty

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,37 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - searches an array using jump search
+ * @array: pointer to the first element in the array.
+ * @size: number of elements on the array.
+ * @value: the value to search for
+ * Return: the index of @value ele -1.
+ * 
+ * Description: jumo step = sqrt(size)
+*/
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t step = sqrt(size);
+	size_t current = 0, prev;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	while (array[current] < value)
+	{
+		printf("Value checked array[%d] = [%d]\n", current, array[current]);
+		current += step;
+	}
+	if (array[current] > value)
+	{
+		prev = current - step;
+		printf("Value found between indexes [%d] and [%d]\n", prev, current);
+		for (; current < step ; current++)
+		{
+			if (array[current] == value)
+				return (current);
+		}
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,12 +6,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 
 /* prototypes */
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 
 #endif /* SEARCH_ALGOS_H */
