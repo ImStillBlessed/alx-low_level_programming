d2303349ee915b801809a4a7e69dda05ad47388f
Oigbochie Blessed <eprince1800@gmail.com>
2024-02-06 21:39:15 +0100

    corrections from chat-GPT 3.5

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,52 +1,37 @@
 #include "search_algos.h"
 
-int helper(int *array, size_t low, size_t high, int value);
-
 /**
- * interpolation_search - searches useing interpolation search.
- * @array: pointer the first element in the ordered list.
- * @size: total elements in the array
- * @value: the value to find in the array.
- * Return: index of @value else -1.
+ * interpolation_search - Searches for a value in a sorted array of integers
+ *                        using interpolation search algorithm.
+ * @array: A pointer to the first element of the array to search in.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: The first index where value is located or -1 if value is not found
+ *         or if array is NULL.
  */
-
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t high = size - 1;
-	size_t low = 0;
+    size_t low = 0, high = size - 1, pos;
 
-	if (array == NULL || size == 0)
-		return (-1);
-	return (helper(array, low, high, value));
-}
+    if (array == NULL || size == 0)
+        return (-1);
 
-/**
- * helper - recursion helper for the interpolitan_search
- * @array: pointer the first element in the ordered list
- * @low: the start f the list
- * @high: the end of the list
- * @value: the value to find in the array.
- * Return: index of @value else -1.
-*/
+    while (low <= high && value >= array[low] && value <= array[high])
+    {
+        pos = low + (((double)(high - low) / (array[high] - array[low]))
+	 * (value - array[low]));
+        printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
-int helper(int *array, size_t low, size_t high, int value)
-{
-	size_t pos;
+        if (array[pos] == value)
+            return (pos);
 
-	if (low <= high && value >= array[low] && value <= array[high])
-	{
-		pos = low 
-		+ (((double)(high - low) / (array[high] - array[low])) 
-		* (value - array[low]));
-		printf("Value checked array[%d] = [%d]\n", pos, array[pos]);
+        if (array[pos] < value)
+            low = pos + 1;
+        else
+            high = pos - 1;
+    }
 
-		if (array[pos] == value)
-			return pos;
-		if (array[pos] < value)
-			return helper(array, pos + 1, high, value);
-		if (array[pos] < value)
-			return helper(array, low, pos - 1, value);
-	}
-	printf("Value checked array[%d] is out of range\n", pos);
-	return (-1);
+    printf("Value checked array[%lu] is out of range\n", pos);
+    return (-1);
 }
